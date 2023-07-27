a1e65827f74ebc51ff9275e66ddc9ac62118118b
Shawn Hoffman <shawn.p.hoffman@gmail.com>
2023-07-27 12:26:57 -0700

    Switch to edge runtime

--- app/layout.js
+++ app/layout.js
@@ -25,6 +25,8 @@ export const metadata = {
 	},
 }
 
+export const runtime = 'edge'
+
 export default function RootLayout({ children }) {
 	return (
 		<html lang="en" style={{ fontFamily: openSans.style.fontFamily }}>
