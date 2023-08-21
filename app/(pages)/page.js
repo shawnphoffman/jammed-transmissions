1167e3ba7cd1b0d935f9992e251e408aeef58f47
Shawn Hoffman <shawn.p.hoffman@gmail.com>
2023-08-21 14:31:45 -0700

    Simplify

--- app/(pages)/page.js
+++ app/(pages)/page.js
@@ -1,13 +1,52 @@
 import { memo } from 'react'
+import { XMLParser } from 'fast-xml-parser'
 
 import styles from 'app/Global.module.css'
-import EpisodesWrapper from 'components/Episodes/EpisodesWrapper'
+import Episodes from 'components/Episodes/Episodes'
 
-const Home = ({}) => {
+const dataUrl = 'https://anchor.fm/s/d8972e20/podcast/rss'
+const xmlOptions = {
+	ignoreAttributes: false,
+	attributeNamePrefix: '@_',
+}
+
+export const revalidate = 60 * 60 * 12
+
+async function getData() {
+	try {
+		var res = await fetch(dataUrl)
+		var xml = await res.text()
+
+		const parser = new XMLParser(xmlOptions)
+		const parsed = parser.parse(xml)
+		const episodes = parsed.rss.channel.item.map(ep => ({
+			guid: ep.guid['#text'],
+			title: ep.title,
+			imgSrc: ep['itunes:image']['@_href'],
+			summary: ep['itunes:summary'],
+			link: ep.link,
+			pubDate: ep.pubDate,
+		}))
+
+		return {
+			raw: parsed.rss.channel.item,
+			episodes,
+		}
+	} catch (error) {
+		return {}
+	}
+}
+
+const Home = async () => {
+	const data = await getData()
+
+	const { episodes } = data
+
+	if (!episodes || episodes.length < 1) return null
 	return (
 		<>
 			<div className={styles.pageDescription}>A positive, listener interactive Star Wars podcast since 2018</div>
-			<EpisodesWrapper />
+			<Episodes episodes={episodes} />
 		</>
 	)
 }
