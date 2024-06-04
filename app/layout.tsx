d2d98803b4cb5cfd84e8cf25828e363939839ee2
Shawn Hoffman <shawn.p.hoffman@gmail.com>
2024-06-04 19:18:42 -0400

    title size

--- app/layout.tsx
+++ app/layout.tsx
@@ -59,7 +59,7 @@ export default function RootLayout({ children }) {
 									priority
 									style={{ '--fa-fade-opacity': 0.85, '--fa-animation-duration': '2s' } as React.CSSProperties}
 								/>
-								<div className="flex flex-row flex-wrap justify-center gap-4">
+								<div className="flex flex-row flex-wrap justify-center gap-x-4 gap-y-1">
 									<ActiveLink href="/" label="Links" />
 									<ActiveLink href="/episodes" label="Episodes" />
 									{process.env.VERCEL_ENV !== 'production' && <ActiveLink href="/updates" label="Updates" fuzzy />}
--- components/updates/PostTitle.tsx
+++ components/updates/PostTitle.tsx
@@ -1,6 +1,6 @@
 export default function PostTitle({ children }) {
 	return (
-		<h1 className="mb-2 text-6xl font-bold bg-gradient-to-b inline-block text-transparent from-white via-brand1 to-brand2 bg-clip-text leading-tight tracking-tighter text-center mt-4  md:text-7xl md:leading-none lg:text-8xl text-balance">
+		<h1 className="inline-block w-full mb-2 text-4xl font-bold leading-tight tracking-tighter text-center text-transparent sm:text-5xl md:mt-4 md:text-6xl bg-gradient-to-b from-white via-brand1 to-brand2 bg-clip-text md:leading-none lg:text-8xl text-balance">
 			{children}
 		</h1>
 	)
