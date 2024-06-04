8ead90f101e6fb2cf77d1ccf83581de0aeadeea0
Shawn Hoffman <shawn.p.hoffman@gmail.com>
2024-06-04 14:47:26 -0400

    cleanup

--- app/(pages)/updates/[slug]/page.tsx
+++ app/(pages)/updates/[slug]/page.tsx
@@ -33,7 +33,6 @@ export default async function PostPage({ params }: PageProps) {
 			<PostCoverImage title={title} image={mainImage} priority />
 
 			<article className="w-full mb-8 text-left rounded-lg bg-slate-950/75">
-				{/* <article className="w-full mb-8 text-left rounded-lg bg-slate-950"> */}
 				<PostBody content={body} />
 			</article>
 		</div>
