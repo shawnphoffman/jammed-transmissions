0b63954ca370fcd0b78522f4f6df7794ad1d2791
Shawn Hoffman <shawn.p.hoffman@gmail.com>
2024-06-05 15:52:16 -0400

    scroll fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 import '@/app/global.css'
-import '@/app/(pages)/pages.css'
 import '@/app/(pages)/icons'
 import '@fortawesome/fontawesome-svg-core/styles.css'
 
@@ -38,38 +37,36 @@ export default function RootLayout({ children }) {
 	return (
 		<html
 			lang="en"
-			className={`${openSans.className} bg-gradient-to-b from-brand-background-dark to-brand-background-light min-h-dvh h-full overflow-hidden p-0 m-0 w-dvw text-white`}
+			className={`${openSans.className} bg-brand-background-dark bg-gradient-to-b from-brand-background-dark to-brand-background-light text-white p-0 m-0 overflow-x-hidden w-dvw`}
 		>
 			<head>
 				<meta name="apple-itunes-app" content="app-id=1445333816" />
 			</head>
-			<body className="h-full overflow-hidden w-dvw">
+			<body className="h-full px-2 py-0 mx-auto my-0 text-white w-dvw min-h-dvh">
 				<StarBackground />
-				<div className="w-full px-4 mx-auto overflow-x-hidden overflow-y-scroll h-dvh">
-					<div className="flex flex-col items-center w-full max-w-screen-xl mx-auto">
-						<div className="z-10 flex flex-col w-full max-w-4xl min-h-dvh">
-							<div className="flex flex-col items-center gap-4 m-4 text-center">
-								<h1 className="sr-only">{title}</h1>
-								<Image
-									className="h-auto max-w-[450px] w-full fa-fade"
-									alt="Jammed Transmissions"
-									src={titleLogo}
-									width={450}
-									height={160}
-									priority
-									style={{ '--fa-fade-opacity': 0.85, '--fa-animation-duration': '2s' } as React.CSSProperties}
-								/>
-								<div className="flex flex-row flex-wrap justify-center gap-x-4 gap-y-1">
-									<ActiveLink href="/" label="Links" />
-									<ActiveLink href="/episodes" label="Episodes" />
-									{process.env.VERCEL_ENV !== 'production' && <ActiveLink href="/updates" label="Updates" fuzzy />}
-									<ActiveLink href="/friends" label="Friends" />
-									<ActiveLink href="/listen-now" label="Listen Now" />
-								</div>
+				<div className="flex flex-col items-center w-full max-w-screen-xl mx-auto">
+					<div className="z-10 flex flex-col w-full max-w-4xl min-h-dvh">
+						<div className="flex flex-col items-center gap-4 m-4 text-center">
+							<h1 className="sr-only">{title}</h1>
+							<Image
+								className="h-auto max-w-[450px] w-full fa-fade"
+								alt="Jammed Transmissions"
+								src={titleLogo}
+								width={450}
+								height={160}
+								priority
+								style={{ '--fa-fade-opacity': 0.85, '--fa-animation-duration': '2s' } as React.CSSProperties}
+							/>
+							<div className="flex flex-row flex-wrap justify-center gap-x-4 gap-y-1">
+								<ActiveLink href="/" label="Links" />
+								<ActiveLink href="/episodes" label="Episodes" />
+								{process.env.VERCEL_ENV !== 'production' && <ActiveLink href="/updates" label="Updates" fuzzy />}
+								<ActiveLink href="/friends" label="Friends" />
+								<ActiveLink href="/listen-now" label="Listen Now" />
 							</div>
-							<div className="flex flex-col items-center flex-1 gap-4 text-center">{children}</div>
-							<Gonk />
 						</div>
+						<div className="flex flex-col items-center flex-1 gap-4 text-center">{children}</div>
+						<Gonk />
 					</div>
 				</div>
 				{process.env.VERCEL_ENV === 'production' && <Analytics />}
