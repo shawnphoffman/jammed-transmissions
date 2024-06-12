3f784167ca7aaa7dddd1b18382b8d31ccb053ec1
Shawn Hoffman <shawn.p.hoffman@gmail.com>
2024-06-11 23:55:46 -0400

    gonk role

--- components/core/Gonk.tsx
+++ components/core/Gonk.tsx
@@ -4,7 +4,7 @@ import GonkImg from './gonk.png'
 
 export default function Gonk() {
 	return (
-		<div className="mt-6 text-center overflow-hidden h-[100px]">
+		<div className="mt-6 text-center overflow-hidden h-[100px]" role="presentation" aria-hidden>
 			<Image src={GonkImg} width="150" height="150" alt="" />
 		</div>
 	)
