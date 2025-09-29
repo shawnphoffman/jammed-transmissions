8172e1a14b37e54b178b4a56d51dc3ec5725c59e
Shawn Hoffman <shawn.p.hoffman@gmail.com>
2025-09-29 10:54:07 -0700

    Filter friends out of updates

--- app/(pages)/updates/page.tsx
+++ app/(pages)/updates/page.tsx
@@ -11,11 +11,13 @@ export const metadata: Metadata = {
 export default async function UpdatesPage() {
 	const posts = await getAllPosts()
 
+	const filteredPosts = posts?.filter(post => post.slug !== 'our-friends') || []
+
 	return (
-		<div className="w-full max-w-screen-lg mb-8 border rounded-lg border-gray-800 bg-gray-950/50">
+		<div className="w-full max-w-screen-lg mb-8 border border-gray-800 rounded-lg bg-gray-950/50">
 			<div className="flex flex-col justify-center w-full p-2 divide-y divide-gray-800">
-				{posts.length === 0 && <div className="my-4 text-2xl font-bold text-center text-brand-shade-1">No posts found</div>}
-				{posts.map(post => {
+				{filteredPosts.length === 0 && <div className="my-4 text-2xl font-bold text-center text-brand-shade-1">No posts found</div>}
+				{filteredPosts.map(post => {
 					return <PostRow key={post._id} post={post as NonNullable<POST_QUERYResult>} />
 				})}
 			</div>
