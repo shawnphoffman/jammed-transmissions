c9f8727cf7f8e878e279fe041678add85b32ef9c
Shawn Hoffman <shawn.p.hoffman@gmail.com>
2026-02-11 20:07:19 -0800

    Update Tailwind configuration for light background color and replace title logo image

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import { Analytics } from '@vercel/analytics/react'
 import { Open_Sans } from 'next/font/google'
 import Image from 'next/image'
 
-import titleLogo from '@/app/title3.png'
+import titleLogo from '@/app/title_du.png'
 import ActiveLink from '@/components/core/ActiveLink'
 import Gonk from '@/components/core/Gonk'
 import StarBackground from '@/components/core/StarBackground'
@@ -33,22 +33,23 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
 	return (
-		<html lang="en" className={`${openSans.className} bg-black text-white p-0 m-0 overflow-x-hidden w-dvw`}>
+		<html lang="en" className={`${openSans.className} bg-brand-background-light text-white p-0 m-0 overflow-x-hidden w-dvw`}>
 			<head>
 				<meta name="apple-itunes-app" content="app-id=1445333816" />
 			</head>
-			<body className="px-2 py-0 mx-auto my-0 text-white min-h-dvh w-dvw bg-gradient-to-b from-brand-background-dark to-brand-background-light">
+			<body className="px-2 py-0 mx-auto my-0 text-white min-h-dvh w-dvw bg-gradient-to-b to-brand-background-dark from-brand-background-light">
 				<StarBackground />
 				<div className="flex flex-col items-center w-full max-w-screen-xl mx-auto">
 					<div className="z-10 flex flex-col w-full max-w-4xl min-h-dvh">
 						<div className="flex flex-col items-center gap-4 m-4 text-center">
 							<h1 className="sr-only">{siteTitle}</h1>
 							<Image
-								className="h-auto max-w-[1000px] w-full fa-fade"
+								// className="h-auto max-w-[1000px] w-full fa-fade"
+								className="h-auto max-w-[700px] w-full fa-fade"
 								alt=""
 								src={titleLogo}
-								width={1000}
-								height={200}
+								width={700}
+								// height={245}
 								priority
 								style={{ '--fa-fade-opacity': 0.85, '--fa-animation-duration': '2s' } as CSSProperties}
 							/>
